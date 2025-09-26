07cfc449eabb3851656aab91ebf8239298aec63f
Dzulani <128301376+dzulanimonyayi@users.noreply.github.com>
2025-09-26 12:03:44 +0200

    Added Files Through Upload

---
+++ src/app/supervisor/supervisor-dashboard/supervisor-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-supervisor-dashboard',
+  imports: [],
+  templateUrl: './supervisor-dashboard.html',
+  styleUrl: './supervisor-dashboard.css'
+})
+export class SupervisorDashboard {
+
+}
